640e0fca3d045bae40ea48220a1e2d6e5b088dbc
AIP-77 <Aleksei-i-p@bk.ru>
2025-01-29 22:19:21 +0500

    Add files via upload

---
+++ final.py
@@ -0,0 +1,17 @@
+# создается словарь с данными заметки
+note = {}
+note['username'] = input('Введите имя пользователя: ')
+note['content'] = input('Введите описание заметки: ')
+note['status'] = input('Введите статус заметки ( например-"ожидает", "готово", "в работе"): ')
+note['created_date'] = input('Введите дату создания заметки в формате "день-месяц-год": ')
+note['issue_date'] = input('Введите дату истечения заметки в формате "день-месяц-год": ')
+note['titles'] = []
+for i in range(3):
+    title = input(f'Введите заголовок заметки {i + 1}: ')
+    note['titles'].append(title)
+
+# Вывод данных заметки
+print("\nСобранная информация о заметке:")
+for key, value in note.items():
+    print(f"{key.capitalize()}: {value}")
+
